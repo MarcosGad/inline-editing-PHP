df60e3c0362569620cbca3b14de59e59d29ac103
MarcosGad <marcosgad@yahoo.com>
2022-01-29 12:47:27 +0200

    enjoy

---
+++ process.php
@@ -0,0 +1,17 @@
+<?php
+include 'database_connection.php';
+
+if(isset($_POST["pk"]))
+{
+	$query = "
+	UPDATE tbl_sample 
+	SET ".$_POST['name']." = '".$_POST["value"]."' 
+	WHERE id = '".$_POST["pk"]."'
+	";
+
+	$connect->query($query);
+
+	echo 'done';
+}
+
+?>
